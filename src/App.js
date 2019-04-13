16475ee3edad056d8d9066a881c9712f9a5c57ad
Phil Hendricks <pfhendri1@gmail.com>
2019-04-13 12:13:39 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,184 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import html2canvas from 'html2canvas';
 import './App.css';
 
+import wallPng from './assets/wall.png'
+import floorWallPng from './assets/wood-floor-with-wall.png'
+import woodFloorPng from './assets/wood-floor.png'
+
+import Grid from './components/Grid/Grid'
+import Controls from './components/Controls/Controls'
+
 class App extends Component {
+  state = {
+    tiles: [
+      { id: 1, image: wallPng, name: 'wall', rotation: 0, count: 0 },
+      { id: 2, image: woodFloorPng, name: 'wood floor', rotation: 0, count: 0 },
+      { id: 3, image: floorWallPng, name: 'floor with wall', rotation: 0, count: 0 }
+    ],
+    grid: [
+      // { id: i, tile: '', src: '' }
+    ],
+    squareSize: 50,
+    currentTile: null,
+    currentSpace: null,
+    action: null,
+    gridWidth: 10,
+    gridHeight: 10,
+    zoom: 100,
+    background: 'grass'
+  }
+
+  changeBackground = (e) => {
+    this.setState({ background: e.target.value })
+  }
+
+  genGrid = (grid) => {
+    this.setState({ grid })
+  }
+
+  selectTile = (e, id) => {
+    this.setState({ currentTile: id, action: null })
+  }
+
+  selectSpace = (e, id) => {
+    // this.setState({ currentSpace: id })
+  }
+
+  clearSpace = (row, column) => {
+    const grid = this.state.grid.map( row => {
+      return row.map( space => {
+        return {...space, tile: {...space.tile} }
+      })
+    })
+    const tile = grid[row][column].tile
+    this.updateTileCount(tile.id, -1)
+    grid[row][column] = { row, column, tile: null }
+    this.setState({ grid })
+  }
+
+  rotateSpace = (row, column) => {
+    const grid = this.state.grid.map( row => {
+      return row.map( space => {
+        return {...space, tile: {...space.tile} }
+      })
+    })
+    const space = { ...grid[row][column], tile: { ...grid[row][column].tile} }
+    space.tile.rotation += 90
+    if (space.tile.rotation > 270) {
+      space.tile.rotation = 0
+    }
+    grid[row][column] = space
+    this.setState({ grid })
+  }
+
+  setTile = (row, column) => {
+    if(this.state.currentTile !== null){
+      let tile = this.state.tiles.find( tile => tile.id === this.state.currentTile )
+      this.updateTileCount(tile.id, 1)
+      const grid = this.state.grid.map( row => {
+        return row.map( space => {
+          return { ...space, tile: {...space.tile} }
+        })
+      })
+      
+      grid[row][column] = { row, column, tile }
+      this.setState({ grid })
+    }else if (this.state.action === 'rotate'){
+      this.rotateSpace(row, column)
+    }else if (this.state.action === 'clear' ){
+      this.clearSpace(row, column)
+    }else{
+      console.log("nothing happened")
+    }
+  }
+
+  updateTileCount = (tileId, change) => {
+    let updatedTile = { ...this.state.tiles.find( tile => tile.id === tileId ) }
+    updatedTile.count += change
+    let tiles = this.state.tiles.map( tile => {
+      if(tile.id == updatedTile.id){
+        return {...updatedTile}
+      }
+      return {...tile}
+    })
+    this.setState({ tiles })
+  }
+
+  setToRotate = () =>{
+    this.setState({ action: 'rotate', currentTile: null })
+  }
+
+  setToClear = () => {
+    this.setState({ action: 'clear', currentTile: null })
+  }
+
+  saveMap = () => {
+    html2canvas(document.querySelector('#grid'))
+      .then( canvas => {
+        const image = canvas.toDataURL('image/jpeg').replace("image/jpeg", "image/octet-stream");
+        console.log(image)
+        const link = document.createElement('a')
+        link.href = image
+        link.download = 'map.png'
+        document.body.appendChild(link)
+        link.click()
+        link.remove()
+      })
+  }
+
+  setMapWidth = (width) => {
+    this.setState({ gridWidth: width })
+  }
+
+  setMapHeight = (height) => {
+    this.setState({ gridHeight: height })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Controls 
+          clearTile={this.setToClear} 
+          rotateTile={this.setToRotate} 
+          saveMap={this.saveMap}
+          gridWidth={this.state.gridWidth}
+          gridHeight={this.state.gridHeight}
+          setWidth={this.setMapWidth}
+          setHeight={this.setMapHeight}
+          action={this.state.action}
+          />
+        <div className="CreationArea">
+          <aside>
+            <select onChange={this.changeBackground}>
+              <option value='grass'>Grass</option>
+              <option value='water'>Water</option>
+            </select>
+            <ul>
+              {this.state.tiles.map(tile =>
+                <li key={tile.id}>
+                  <img 
+                    src={tile.image} 
+                    alt={tile.name} 
+                    className={this.state.currentTile === tile.id ? 'active' : ''}
+                    onClick={(e) => this.selectTile(e, tile.id)}
+                    />
+                </li>
+              )}
+            </ul>
+          </aside>
+          <Grid 
+            squareSize={this.state.squareSize} 
+            grid={this.state.grid} 
+            updateGrid={this.genGrid} 
+            setTile={this.setTile}
+            selectSpace={this.selectSpace}
+            gridWidth={this.state.gridWidth}
+            gridHeight={this.state.gridHeight}
+            zoom={this.state.zoom}
+            background={this.state.background}
+            />
+        </div>
       </div>
     );
   }
---
+++ src/components/Grid/Grid.jsx
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import './Grid.css'
+import Grass from '../../assets/grass.png'
+import Water from '../../assets/water.jpg'
+class Grid extends Component {
+  componentDidMount() {
+    this.genGrid()
+  }
+
+  componentDidUpdate(prevProps){
+    if(prevProps.gridHeight !== this.props.gridHeight || 
+        prevProps.gridWidth !== this.props.gridWidth){
+      this.updateGrid()
+    }
+  }
+
+  updateGrid = () => {
+    let grid = this.props.grid.map(row => {
+      if(row.length > this.props.gridWidth){
+        row = row.slice(0, this.props.gridWidth)
+      }
+      return row.map( item => {
+        return {...item} 
+      })
+    })
+
+    let newGrid = []
+    for(let i = 0; i < this.props.gridHeight; i++){
+      let row = []
+      if(!grid[i]){
+        let row = []
+        for(let j = 0; j < this.props.gridWidth; j++){
+          row.push({ row: i, column: j, tile: null })
+        }
+        newGrid.push(row)
+      } else if(grid[i].length < this.props.gridWidth ){
+        for(let j = 0; j < this.props.gridWidth; j++){
+          if(!grid[i][j]){
+            grid[i].push({ row: i, column: j, tile: null })
+          }
+        }
+        row = grid[i]
+      } else{
+        row = grid[i]
+      }
+      newGrid.push(row)
+    }
+    this.props.updateGrid(newGrid)
+  }
+
+  genGrid = () => {
+    const squaresWide = this.props.gridWidth
+    const squaresHigh = this.props.gridHeight
+    
+    let grid = []
+    for(let j = 0; j < squaresHigh; j++){
+      let row = []
+      for (let i = 0; i < squaresWide; i++) {
+        row.push({ row: j, column: i, tile: null })
+      }
+      grid.push(row)
+    }
+    this.props.updateGrid(grid)
+  }
+
+  handleMouseOver = (e, row, column) => {
+    e.preventDefault()
+    if(e.buttons === 1){
+      this.props.selectSpace(row, column)
+      this.props.setTile(row, column)
+    }
+  }
+
+  handleMouseDown = (e, row, column) => {
+    e.preventDefault();
+    this.props.selectSpace(row, column)
+    this.props.setTile(row, column)
+  }
+
+  setBackground = () => {
+    if(this.props.background === 'grass'){
+      return Grass
+    }else if (this.props.background === 'water'){
+      return Water
+    }
+  }
+
+  render() {
+    const genGrid = this.props.grid.map((row) => {
+      return row.map((space, index) => (
+          <div
+          key={index}
+          className={`tile rotate-${space.tile !== null ? space.tile.rotation : ''}`}
+          style={{
+            backgroundImage: space.tile !== null ? `url('${space.tile.image}')` : '',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            height: this.props.squareSize,
+            width: this.props.squareSize
+          }}
+          onMouseDown={(e) => this.handleMouseDown(e, space.row, space.column)}
+          onMouseOver={(e) => this.handleMouseOver(e, space.row, space.column)}></div>
+        ))
+      })
+    return (
+      <main >
+        <div id='grid' style={
+          { 
+            gridTemplateColumns: `repeat(${this.props.gridWidth}, ${this.props.squareSize}px)`,
+            gridTemplateRows: `repeat(${this.props.gridHeight}, ${this.props.squareSize}px)`,
+            display: 'grid',
+            width: `${this.props.gridWidth * this.props.squareSize}px`,
+            zoom: `${this.props.zoom}%`,
+            backgroundImage: `url(${this.setBackground()})`,
+          }
+        }>
+          {genGrid}
+        </div>
+      </main>
+    );
+  }
+}
+
+export default Grid;
