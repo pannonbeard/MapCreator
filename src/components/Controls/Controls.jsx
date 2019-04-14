fdd57fc0a1d8a1559879a7383cbd7fc7fe182e1f
Phil Hendricks <pfhendri1@gmail.com>
2019-04-14 10:37:16 -0500

    Made Controls float over body to fit design

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -6,10 +6,8 @@ class Controls extends Component {
     return (
       <div id='controls'>
         <button type='button' className={this.props.action === 'rotate' ? 'active' : ''} onClick={this.props.rotateTile}>Rotate Tile</button>
-        <button type='button' className={this.props.action === 'clear' ? 'active' : ''} onClick={this.props.clearTile}>Clear Tile</button>
         <button type='button' onClick={this.props.saveMap}>Save Map</button>
-        <label>Width: <input type='number' value={this.props.gridWidth} onChange={ (e) => this.props.setWidth(e.target.value)}/></label>
-        <label>Height: <input type='number' value={this.props.gridHeight} onChange={ (e) => this.props.setHeight(e.target.value)} /></label>
+        <button type='button' className={this.props.action === 'clear' ? 'active' : ''} onClick={this.props.clearTile}>Clear Tile</button>
       </div>
     );
   }
